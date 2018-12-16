99b1dc23f1ee77dab332cdb15573139049b8bd9c
morwen80 <ribesnero80@gmail.com>
2018-12-16 16:15:03 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  # if we add :update on the array above, this line can be deleted, since
+  # it creates the same thing
+  patch 'posts/:id', to: 'posts#update'
 end
